8f57c5bd2c9c162f52bcce2cefa3d69318f57692
Woo-dy <kim03189@naver.com>
2025-02-09 20:22:41 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// MAIN 01. 검색창 클릭 시 '통합검색' 및 크기 변경 script
-
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click',  function () {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-
-searchInputEl.addEventListener('blur', function() {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 // MAIN 02. badges scroll 배너 사라지고 나타나는 효과
 
 const badgeEl = document.querySelector('header .badges');
@@ -177,8 +157,3 @@ spyEls.forEach(function (spyEl) {
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller());
 });
-
-
-//올해가 몇년도인지 추출하는 방법
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 올해가 몇년도인지 추출
